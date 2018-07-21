20dc9c3a513eb12fe292642571ecf60e6198ce01
Matthias Felleisen <matthias@ccs.neu.edu>
2018-07-21 12:22:47 -0400

    test syntax

--- Lib/struct-with.rkt
+++ Lib/struct-with.rkt
@@ -12,18 +12,28 @@
 ;; -----------------------------------------------------------------------------
 (require (for-syntax syntax/parse))
 (require (for-syntax racket/syntax))
+(module+ test (require rackunit))
+(module+ test (require syntax/macro-testing))
 
 ;; -----------------------------------------------------------------------------
 (define-syntax (struct-with stx)
   (syntax-parse stx
     [(_ name (field ...) stuff ...)
-     (with-syntax ([with-board (format-id stx "with-~a" (syntax-e #'name))])
+     (with-syntax ([with-board (format-id stx "with-~a" (syntax-e #'name))]
+                   [name?      (format-id stx "~a?" (syntax-e #'name))])
        #'(begin
            (struct name (field ...) stuff ...)
            (define-syntax (with-board stx)
              (syntax-parse stx
                [(_ n:expr e (... ...))
                 (with-syntax ([field (datum->syntax stx 'field)] ...)
-                  #'(let ()
-                      (match-define (name field ...) n)
+                  #'(let ([v n])
+                      (unless (name? v)
+                        (error 'with-board "not a ~a: ~e" 'name v))
+                      (match-define (name field ...) v)
                       e (... ...)))]))))]))
+
+(module+ test
+  (struct-with foo ())
+  (check-equal? (with-foo (foo) 0) 0)
+  (check-exn exn:fail? (lambda () (convert-compile-time-error  (with-foo 0 (foo))))))
