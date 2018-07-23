bd74dd13c1419323c0ced4d12e895fbe288f5c70
Matthias Felleisen <matthias@ccs.neu.edu>
2018-07-22 21:34:48 -0400

    get with to work

--- Lib/struct-with.rkt
+++ Lib/struct-with.rkt
@@ -1,42 +1,52 @@
 #lang racket
 
-;; -----------------------------------------------------------------------------
+;; ---------------------------------------------------------------------------------------------------
 (provide
  ;; SYNTAX
- ;; (struct-with name (field ...) option ...)
- ;; defines
- ;; (1) a (struct name (field ...) option ...)
- ;; (2) a (with-name n:expr element ...) syntax for destructuring a struct name
- struct-with)
+ ;; (with struct:id s-instance:expr e ...)
+ ;; struct refers a structure type, s-instance evaluates to an instance of struct
+ ;; binds the field names of struct in e ... to the fields of s-instance 
+ with)
 
-;; -----------------------------------------------------------------------------
+;; ---------------------------------------------------------------------------------------------------
+(require (for-syntax racket/struct-info))
 (require (for-syntax syntax/parse))
-(require (for-syntax racket/syntax))
+(require (for-syntax racket/list))
+
 (module+ test (require rackunit))
 (module+ test (require syntax/macro-testing))
 
-;; -----------------------------------------------------------------------------
-(define-syntax (struct-with stx)
+;; ---------------------------------------------------------------------------------------------------
+(define-syntax (with stx)
   (syntax-parse stx
-    [(_ name (field ...) stuff ...)
-     (with-syntax ([with-board (format-id stx "with-~a" (syntax-e #'name))]
-                   [name?      (format-id stx "~a?" (syntax-e #'name))])
-       #'(begin
-           (struct name (field ...) stuff ...)
-           (define-syntax (with-board stx)
-             (syntax-parse stx
-               [(_ n:expr e (... ...))
-                (with-syntax ([field (datum->syntax stx 'field)] ...)
-                  (syntax/loc stx
-                    (let ([v n])
-                      (unless (name? v)
-                        (error 'with-board "not a ~a: ~e" 'name v))
-                      (match-define (name field ...) v)
-                      e (... ...))))]))))]))
+    [(_ structure-type:id instance:expr e ...)
+     (define-values (predicate accessors fields) (structure-type->predicate+accessors+fields stx))
+     (with-syntax ([predicate?     predicate]
+                   [(accessor ...) accessors]
+                   [(field ...)    fields])
+       #`(let* ([v instance]
+                [_ (unless (predicate? v)
+                     (error 'with "expected struct instance of type ~a, given ~e" 'structure-type v))]
+                [field (accessor v)] ...)
+           e ...))]))
+
+(define-for-syntax (structure-type->predicate+accessors+fields stx)
+  (define s:type (syntax-parse stx [(_ structure-type . x) #'structure-type]))
+  (define s:info (extract-struct-info (syntax-local-value s:type (not-a-structure-type s:type))))
+  (define-values (_ constructor predicate accessors _2 _3) (apply values s:info))
+  (define accessor-field-pattern (string-append (symbol->string (syntax-e s:type)) "-(.*)"))
+  (define fields
+    (for/list ((a:stx accessors))
+      (define a:sym (symbol->string (syntax-e a:stx)))
+      (define f:str (regexp-match accessor-field-pattern a:sym))
+      (define field (datum->syntax stx (string->symbol (second f:str))))
+      field))
+  (values predicate accessors fields))
+
+(define-for-syntax ((not-a-structure-type stype))
+  (raise-syntax-error #f "not a structure type" stype))
 
 (module+ test
-  (struct-with foo (bar))
-
-  (check-equal? (with-foo (foo 1) 0) 0)
-  (check-equal? (with-foo (foo 1) bar) 1)
-  (check-exn exn:fail? (lambda () (convert-compile-time-error  (with-foo 0 (foo))))))
+  (struct foo (bar moo))
+  (check-equal? (with foo (foo 10 20) (+ moo bar)) 30)
+  (check-exn exn:fail:syntax? (lambda () (convert-compile-time-error (with bar (foo 20 0) 0)))))
