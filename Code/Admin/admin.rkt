f2c530c2953528e06c6ba90d1a278e67f906fe91
Matthias Felleisen <matthias@ccs.neu.edu>
2018-08-11 12:39:29 -0400

    MAJOR DESIGN FLAW: workers should not come with locations at all, fixed

--- Code/Admin/admin.rkt
+++ Code/Admin/admin.rkt
@@ -1,8 +1,26 @@
 #lang racket
 
+;; The Referee plays a single game for two players.
+;; The class is instanitated with two players. 
+
+(define player/c
+  (object/c
+   (init-field name)
+   (placement (->m (listof (list/c in-range? in-range?)) (list/c in-range? in-range?)))
+   (take-turn (->m board? action?))))
+   
+
+(provide
+ (contract-out
+  (admin%
+   (class/c
+    (init-field (one player/c) (two player/c))
+    (play (->m string?))))))
+
+
 ;; ---------------------------------------------------------------------------------------------------
-(require "../Common/rule-checking.rkt")
 (require "../Player/player.rkt")
+(require "../Common/worker.rkt")
 (require "../Common/board.rkt")
 (require "../Common/actions.rkt")
 
@@ -18,26 +36,31 @@
 
     (send one other two-name)
     (send two other one-name)
-
-    (define/private (placement target lot)
-      (define new-location (send target placement lot))
-      (and (not (member new-location lot)) new-location))
-
-    ;; -> (U String [Listof Worker])
+    
+    ;; -> (U Board String)
     (define/private (setup)
       (let/ec done
-        (define-values (_ workers)
-          (for/fold ([place* '()][worker* '()]) ([n '(1 2 3 4)])
-            (define-values (player name) (if (odd? n) (values one one-name) (values two two-name)))
-            (define new-place (placement player place*))
-            (if (boolean? new-place)
-                (done (format "~a broke the rules of placing workers" name))
-                (values (cons new-place place*) (cons (apply worker name new-place) worker*)))))
-        (apply init workers)))
+        (let*-values ([(                lot) '()]
+                      [(player1-worker1 lot) (placement one one-name 1 done lot)]
+                      [(player1-worker2 lot) (placement one one-name 2 done lot)]
+                      [(player2-worker1 lot) (placement two two-name 1 done lot)]
+                      [(player2-worker2 lot) (placement two two-name 2 done lot)])
+          (init player1-worker1 player1-worker2 player2-worker1 player2-worker2))))
 
+    #; (Player<%> String [String -> Empty] [Listof [List N N]] ->
+                  (values (List Worker [List N N]) [Listof [List N N]]))
+    ;; get a placement for a specific worker from target
+    ;; EFFECT escape with done if target produces a location that is already occupied 
+    (define/private (placement target target-name worker# done lot)
+      (define new-location (send target placement lot))
+      (when (member new-location lot)
+        (done (format "~a broke the rules of placing workers" target-name)))
+      (define full-name (format "~a~a" target-name worker#))
+      (values (cons (worker full-name) new-location) (cons new-location lot)))
+      
     (field [board (setup)])
 
-    ;; -> (U String)
+    ;; -> String
     ;; determine the winner (and the loser)
     (define/public (play)
       (if (string? board) board (play-rounds board)))
@@ -47,7 +70,7 @@
 
     (define/private (play-rounds board (one one) (two two))
       (define a (send one take-turn board))
-      (displayln a)
+      (displayln (apply-action board a))
       (if (not (check-action board a))
           (report (get-field name two) (get-field name one) a)
           (match a
@@ -61,6 +84,7 @@
 
 ;; ---------------------------------------------------------------------------------------------------
 (module+ test
+  (require (submod ".."))
   (define admin
     (new admin%
          [one (new player% [name "mf"])]
