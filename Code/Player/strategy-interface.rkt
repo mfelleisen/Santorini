3a7ea08de8cce8162b5143eb2b323bd57798517f
Matthias Felleisen <matthias@ccs.neu.edu>
2018-08-17 13:59:04 -0400

    check the protocol (NSF), with testing

--- Code/Player/strategy-interface.rkt
+++ Code/Player/strategy-interface.rkt
@@ -11,6 +11,8 @@
 (require "../Common/board.rkt")
 (require "../Common/actions.rkt")
 (require "../Common/player-interface.rkt")
+(module+ test (require rackunit))
+
 
 ;; ---------------------------------------------------------------------------------------------------
 (define (name/c b) (and/c string? (on? b)))
@@ -25,23 +27,7 @@
    (initialization
     ;; pick the next place for a worker
     ;; called twice in alternating ways, but 'other' made a placement in between 
-    (->i ((this any/c)
-          (lop  (this) (and/c placements/c
-                              (lambda (placements)
-                                (define me (get-field player this))
-                                (cond
-                                  ;; second call: 
-                                  [(and (>= (string-length me) 2) (string=? (substring me 0 2) "1-"))
-                                   (define old-me (substring me 2))
-                                   (set-field! player this old-me)
-                                   (define you (get-field other this))
-                                   (and (placed-at-least-one placements old-me)
-                                        (placed-at-least-one placements you))]
-                                  ;; first call: 
-                                  [else
-                                   (set-field! player this (string-append "1-" me))
-                                   (not (placed-at-least-one placements me))])))))
-         (r place/c)))
+    (->i ((this any/c) (lop  (this) (and/c placements/c (placement-protocol/c this)))) (r place/c)))
    
    (take-turn 
     ;;  pick the next action
@@ -50,16 +36,42 @@
 
 (define strategy/c (instanceof/c strategy%/c))
 
-(define (tee x) (displayln x) x)
-
-;; Strategy N -> [Placements -> (U Boolean APlacement)]
+(define ((placement-protocol/c this) placements)
+  (define me (get-field player this))
+  (cond
+    ;; second call: 
+    [(and (>= (string-length me) 2) (string=? (substring me 0 2) "1-"))
+     (define old-me (substring me 2))
+     (set-field! player this old-me)
+     (define you (get-field other this))
+     (and (placed-at-least-one placements old-me)
+          (placed-at-least-one placements you))]
+    ;; first call: 
+    [else
+     (set-field! player this (string-append "1-" me))
+     (not (placed-at-least-one placements me))]))
 
 (define (placed-at-least-one placements you)
   (for/first ((p placements) #:when (string=? (first p) you)) p))
 
-
-
+;; ---------------------------------------------------------------------------------------------------
 (module+ test
-  (require rackunit)
   (check-false  (placed-at-least-one '() "cd"))
-  (check-equal? (placed-at-least-one `(("cd" 0 0)) "cd") '("cd" 0 0)))
+  (check-equal? (placed-at-least-one `(("cd" 0 0)) "cd") '("cd" 0 0))
+
+  (define/contract mock-strategy% strategy%/c
+    (class object% (init-field player other)
+      (super-new)
+      (define/public (initialization lop) (list 0 0))
+      (define/public (take-turn b) (giving-up player))))
+  
+  (define (protocoled-mocked-strategy)
+    (define mock-strategy (new mock-strategy% [player "x"][other "o"]))
+    (placement-protocol/c mock-strategy))
+  
+  (check-true   ((protocoled-mocked-strategy) '()))
+  (check-equal? (let ([s (protocoled-mocked-strategy)])
+                  (s '()) ;; first, irrelevant call 
+                  (s '(("o" 1 1) ("x" 1 2))))
+                '("o" 1 1)))
+
