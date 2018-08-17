0ee31ca765dd8d0cbcfbf784cf62f7aac53dc4a8
Matthias Felleisen <matthias@ccs.neu.edu>
2018-08-17 12:10:45 -0400

    check the protocol (NSF)

--- Code/Player/strategy-interface.rkt
+++ Code/Player/strategy-interface.rkt
@@ -17,11 +17,31 @@
 
 (define strategy%/c
   (class/c
-   (init-field (player string?) (other string?))
+
+   (init-field
+    (player string?)
+    (other  string?))
+   
    (initialization
     ;; pick the next place for a worker
-    ;; called twice 
-    (->i ((this any/c) (l placements/c)) (r place/c)))
+    ;; called twice in alternating ways, but 'other' made a placement in between 
+    (->i ((this any/c)
+          (lop  (this) (and/c placements/c
+                              (lambda (placements)
+                                (define me (get-field player this))
+                                (cond
+                                  ;; second call: 
+                                  [(and (>= (string-length me) 2) (string=? (substring me 0 2) "1-"))
+                                   (define old-me (substring me 2))
+                                   (set-field! player this old-me)
+                                   (define you (get-field other this))
+                                   (and (placed-at-least-one placements old-me)
+                                        (placed-at-least-one placements you))]
+                                  ;; first call: 
+                                  [else
+                                   (set-field! player this (string-append "1-" me))
+                                   (not (placed-at-least-one placements me))])))))
+         (r place/c)))
    
    (take-turn 
     ;;  pick the next action
@@ -29,3 +49,17 @@
     (->i ((this any/c) (b board?)) (r action?)))))
 
 (define strategy/c (instanceof/c strategy%/c))
+
+(define (tee x) (displayln x) x)
+
+;; Strategy N -> [Placements -> (U Boolean APlacement)]
+
+(define (placed-at-least-one placements you)
+  (for/first ((p placements) #:when (string=? (first p) you)) p))
+
+
+
+(module+ test
+  (require rackunit)
+  (check-false  (placed-at-least-one '() "cd"))
+  (check-equal? (placed-at-least-one `(("cd" 0 0)) "cd") '("cd" 0 0)))
--- Code/Player/strategy.rkt
+++ Code/Player/strategy.rkt
@@ -25,11 +25,11 @@
     (super-new)
     
     (define/public (initialization list-of-places)
-      (initialization1 list-of-places))
+      (init-to-as-far-away-as-possible list-of-places))
 
     ;; [Listof [List String N N]] -> [List N N]
     ;; maximize distance from other players 
-    (define/private (initialization1 lop)
+    (define/private (init-to-as-far-away-as-possible lop)
       (cond
         [(empty? lop) (list 0 0)]
         [else
@@ -70,28 +70,58 @@
 ;; ---------------------------------------------------------------------------------------------------
 (module+ test
   (require (submod ".."))
-
+  
   (check-equal? (distance (list 3 4) (list 0 0)) 5)
   (check-equal? (distance (list 12 5) (list 9 1)) 5)
   
-  (define xsafe (new safe-strategy% [player "x"] [other "o"]))
-  (define osafe (new safe-strategy% [player "o"] [other "x"]))
+  (define (make-safe x o) (new safe-strategy% [player x] [other o]))
 
-  (check-pred cons? (send xsafe initialization '()) "just to make sure that the mechanics work out")
-  (check-equal? (send xsafe initialization '()) (list 0 0))
-  (check-equal? (send xsafe initialization (list (list "o" 0 0))) (list 5 5))
-  (check-equal? (send xsafe initialization `(("o" 0 0)("x" 5 5))) (list 4 5))
-  (check-equal? (send osafe initialization `(("x" 4 5)("o" 0 0)("x" 5 5))) (list 1 0))
+  ;; -------------------------------------------------------------------------------------------------
+  ;; initialization 
+  (check-pred cons? (send (make-safe "x" "o") initialization '()) "the mechanics work out")
+  
+  (define x-o-safe (make-safe "x" "o"))
+  (check-equal? (send x-o-safe initialization '()) (list 0 0))
+  (check-equal? (send x-o-safe initialization `(("x" 1 1)("o" 0 0))) (list 5 5))
 
+  (define x-o-snd-call-safe (make-safe "x" "o"))
+  (check-equal? (begin
+                  (send x-o-snd-call-safe initialization '())
+                  (send x-o-snd-call-safe initialization `(("o" 0 0)("x" 5 5))))
+                (list 4 5))
+
+  (define o-x--snd-call-safe (make-safe "o" "x"))
+  (check-equal? (begin
+                  (send o-x--snd-call-safe initialization '())
+                  (send o-x--snd-call-safe initialization `(("x" 4 5)("o" 0 0)("x" 5 5))))
+                (list 1 0))
+
+  (define-syntax-rule (check-protocol lop1 lop2 ... msg)
+    (check-exn exn:fail:contract?
+             (lambda ()
+               (define x-o (make-safe "x" "o"))
+               (send x-o initialization 'lop1)
+               (send x-o initialization 'lop2) ...)
+             msg))
+
+  (check-protocol (("x" 1 1)) "first call, but already placed a token")
+  (check-protocol (("o" 1 1)) (("o" 1 1)) "second call, 'I' have not placed a token yet")
+  (check-protocol (("o" 1 1)) (("x" 1 1)) "second call, 'other' has not placed a token yet")
+
+  ;; -------------------------------------------------------------------------------------------------
+  ;; take turn 
   (define o1 (worker "o1"))
   (define gu-mb-board
     (cboard 
      [[1x1 2o1]
       [2x2 1o2]
       [4   4]]))
-  
-  (check-equal? (send xsafe take-turn gu-mb-board) (giving-up "x"))
-  (check-equal? (send osafe take-turn gu-mb-board) (move-build o1 EAST SOUTH EAST SOUTH))
+
+  (define x-o-take-turn (make-safe "x" "o"))
+  (define o-x-take-turn (make-safe "o" "x"))
+
+  (check-equal? (send x-o-take-turn take-turn gu-mb-board) (giving-up "x"))
+  (check-equal? (send o-x-take-turn take-turn gu-mb-board) (move-build o1 EAST SOUTH EAST SOUTH))
 
   (define winning-board
     (cboard
@@ -99,7 +129,7 @@
       [2x2 1o2]
       [4   4]]))
 
-  (check-equal? (send osafe take-turn winning-board) (winning-move o1 EAST PUT))
+  (check-equal? (send o-x-take-turn take-turn winning-board) (winning-move o1 EAST PUT))
 
   (define gu-2-down-board
     (cboard 
@@ -108,4 +138,4 @@
       [2   4]
       [4   4]]))
 
-  (check-equal? (send xsafe take-turn gu-2-down-board) (giving-up "x")))
+  (check-equal? (send x-o-take-turn take-turn gu-2-down-board) (giving-up "x")))
