a717e033f7a3934a16d4f7fd55bfd057a1b4daaf
Matthias Felleisen <matthias@ccs.neu.edu>
2018-10-25 18:10:59 -0400

    name infinite loops, go into loop in second game

--- Code/Player/failing-inf-placement.rkt
+++ Code/Player/failing-inf-placement.rkt
@@ -2,4 +2,5 @@
 
 ;; provide a player that goes into an infinite loop during 1st placement
 (require "failing.rkt")
-(failing-module 1 #:p-failure (lambda (l) (let loop () (loop))))
+(define inf-pl (lambda (l) (let loop () (loop))))
+(failing-module 1 #:p-failure inf-pl)
--- Code/Player/failing-inf-turn.rkt
+++ Code/Player/failing-inf-turn.rkt
@@ -2,4 +2,5 @@
 
 ;; provide a player that goes into an infinite loop after 2 correct turns
 (require "failing.rkt")
-(failing-module 2 #:tt-failure (lambda (board) (let loop () (loop))))
+(define inf-tt (lambda (board) (let loop () (loop))))
+(failing-module 1 #:tt-failure inf-tt)
